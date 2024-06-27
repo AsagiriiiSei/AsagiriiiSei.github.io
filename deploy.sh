0f78977e97720d8cf7039e1976c3d6e23c0da01f
AsagiriiiSei <AsagiriiiSei@gmail.com>
2024-06-27 14:33:07 +0800

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+pnpm run doc:build
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy: 发布到 gh-pages [ci skip]'
+
+git push -f git@github.com:wubaiqing/zaobao.git master:gh-pages
+
+cd -
